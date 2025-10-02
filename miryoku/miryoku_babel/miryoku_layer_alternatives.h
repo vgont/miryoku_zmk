831f8f9879031249ce4b68bf2fe3b00c36f3707f
vgont <vgontdev@gmail.com>
2025-10-02 00:17:59 -0300

    feat: add isaac layer

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -385,3 +385,9 @@ U_UND,             U_CUT,             U_CPY,             U_PST,             U_RD
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,              U_NU,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,            U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+
+#define MIRYOKU_ALTERNATIVES_ISAAC \
+&kp LCTRL,             &kp Q,             &kp W,             &kp E,             U_NA,             U_NA,             U_NA,             &kp UP,             U_NA,             U_NA,           \
+&kp TAB,             &kp A,             &kp S,             &kp D,             U_NA,             U_NA,             &kp LEFT,           &kp DOWN,             &kp RIGHT,             U_NA,           \
+&kp LSHFT,             U_NA,             U_NA,             U_NA,             U_NA,             U_NA,             U_NA,             U_NA,             U_NA,             U_NA,           \
+U_NP,             U_NP, &kp ESC,             &kp SPACE,             &kp TAB,             &kp RET,             &kp BSPC,          &kp DEL,              U_NP,             U_NP,           \
