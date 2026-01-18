b59cafadd79e5924940a93fe1f2e6bc1c11b30c1
vgont <vgontdev@gmail.com>
2026-01-18 16:50:15 -0300

    fix: layer list

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -6,18 +6,18 @@
 
 #if !defined(MIRYOKU_LAYER_LIST)
 
-#define MIRYOKU_LAYER_LIST                                                     \
-  MIRYOKU_X(BASE, "Base")                                                      \
-  MIRYOKU_X(EXTRA, "Extra")                                                    \
-  MIRYOKU_X(TAP, "Tap")                                                        \
-  MIRYOKU_X(BUTTON, "Button")                                                  \
-  MIRYOKU_X(NAV, "Nav")                                                        \
-  MIRYOKU_X(MOUSE, "Mouse")                                                    \
-  MIRYOKU_X(MEDIA, "Media")                                                    \
-  MIRYOKU_X(NUM, "Num")                                                        \
-  MIRYOKU_X(SYM, "Sym")                                                        \
-  MIRYOKU_X(FUN, "Fun")                                                        \
-  MIRYOKU_X(QWERTY_WITH_ARROWS, "QWERTY_WITH_ARROWS")
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(QWERTY_WITH_ARROWS, "QWERTY_WITH_ARROWS")
 
 #define U_BASE 0
 #define U_EXTRA 1
