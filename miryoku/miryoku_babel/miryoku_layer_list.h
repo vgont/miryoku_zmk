1dd7f9353de5b57c8e1dac9f4690ee62b679be69
vgont <vgontdev@gmail.com>
2026-01-18 16:35:09 -0300

    feat: Add qwerty_with_arrows layer

--- miryoku/miryoku_babel/miryoku_layer_list.h
+++ miryoku/miryoku_babel/miryoku_layer_list.h
@@ -4,29 +4,31 @@
 
 #pragma once
 
-#if !defined (MIRYOKU_LAYER_LIST)
+#if !defined(MIRYOKU_LAYER_LIST)
 
-#define MIRYOKU_LAYER_LIST \
-MIRYOKU_X(BASE,   "Base") \
-MIRYOKU_X(EXTRA,  "Extra") \
-MIRYOKU_X(TAP,    "Tap") \
-MIRYOKU_X(BUTTON, "Button") \
-MIRYOKU_X(NAV,    "Nav") \
-MIRYOKU_X(MOUSE,  "Mouse") \
-MIRYOKU_X(MEDIA,  "Media") \
-MIRYOKU_X(NUM,    "Num") \
-MIRYOKU_X(SYM,    "Sym") \
-MIRYOKU_X(FUN,    "Fun")
+#define MIRYOKU_LAYER_LIST                                                     \
+  MIRYOKU_X(BASE, "Base")                                                      \
+  MIRYOKU_X(EXTRA, "Extra")                                                    \
+  MIRYOKU_X(TAP, "Tap")                                                        \
+  MIRYOKU_X(BUTTON, "Button")                                                  \
+  MIRYOKU_X(NAV, "Nav")                                                        \
+  MIRYOKU_X(MOUSE, "Mouse")                                                    \
+  MIRYOKU_X(MEDIA, "Media")                                                    \
+  MIRYOKU_X(NUM, "Num")                                                        \
+  MIRYOKU_X(SYM, "Sym")                                                        \
+  MIRYOKU_X(FUN, "Fun")                                                        \
+  MIRYOKU_X(QWERTY_WITH_ARROWS, "QWERTY_WITH_ARROWS")
 
-#define U_BASE   0
-#define U_EXTRA  1
-#define U_TAP    2
+#define U_BASE 0
+#define U_EXTRA 1
+#define U_TAP 2
 #define U_BUTTON 3
-#define U_NAV    4
-#define U_MOUSE  5
-#define U_MEDIA  6
-#define U_NUM    7
-#define U_SYM    8
-#define U_FUN    9
+#define U_NAV 4
+#define U_MOUSE 5
+#define U_MEDIA 6
+#define U_NUM 7
+#define U_SYM 8
+#define U_FUN 9
+#define U_QWERTY_WITH_ARROWS 10
 
 #endif
